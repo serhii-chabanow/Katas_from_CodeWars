6cf39d56bb415f2274ddea7aefa3a15f124e9ea7
Serhii Chabanow <serhii.chabanow.work@gmail.com>
2024-06-11 11:12:44 +0300

    test

--- Greed_is_Good.py
+++ Greed_is_Good.py
@@ -1,3 +1,19 @@
-# This is a sample Python script.
+def score(dice):
+    result_points = 0
+    count_dict = {}
 
-...
+    for elem in dice:
+        if elem in count_dict:
+            count_dict[elem] += 1
+        else:
+            count_dict[elem] = 1
+
+    for num, count in count_dict.items():
+        if num == 1:
+            result_points += (count // 3) * 1000 + (count % 3) * 100
+        elif num == 5:
+            result_points += (count // 3) * 500 + (count % 3) * 50
+        else:
+            result_points += (count // 3) * num * 100
+
+        return result_points
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Katas_from_CodeWars
+# Kata's from CodeWars
+
+So here I store my completed katas from CodeWars from the recent past, though not all of them. 
+Most of these were done in Python. 
+Each separate file represents a different task.
